a891c7dca84736d638bc49c0154ea256ca47f495
ng <nick.goupinets@gmail.com>
2019-03-23 16:50:37 -0400

    Updates

---
+++ precache-manifest.b4509932c6c894f8e56f8364512fe194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35a9fc2d952ff4c77a1",
+    "url": "/pokemonfriends/build/static/js/runtime~main.a35a9fc2.js"
+  },
+  {
+    "revision": "0f13a290f47d118f7d2a",
+    "url": "/pokemonfriends/build/static/js/main.0f13a290.chunk.js"
+  },
+  {
+    "revision": "811fa40c5bb596ece27f",
+    "url": "/pokemonfriends/build/static/js/2.811fa40c.chunk.js"
+  },
+  {
+    "revision": "0f13a290f47d118f7d2a",
+    "url": "/pokemonfriends/build/static/css/main.b3f8b4f5.chunk.css"
+  },
+  {
+    "revision": "811fa40c5bb596ece27f",
+    "url": "/pokemonfriends/build/static/css/2.31de2960.chunk.css"
+  },
+  {
+    "revision": "e37dd8d1cdae70b8a038127a27fedcbb",
+    "url": "/pokemonfriends/build/index.html"
+  }
+];
